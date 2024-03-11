4a467a2359ecfe8f73d9e3b1cecd1454fbb98a7b
Hillario <hillcheswiztyphoon@gmail.com>
2024-03-11 12:26:21 +0300

    created a new plugin

---
+++ wp-content/plugins/saro-cf/saro-cf.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Plugin Name
+ *
+ * @package           Saro-CF
+ * @author            Hillary Chesaro
+ * @copyright         2024 Saro Labs
+ * @license           Saro Labs
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Saro-CF
+ * Plugin URI:        https://hillario.github.io
+ * Description:       Saro-CF is a comprehensive crowdfunding plugin designed specifically for WordPress websites. With Saro-CF, users can easily create and manage crowdfunding campaigns, empowering them to raise funds for their projects, causes, or initiatives directly from their WordPress site.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Hillary Chesaro
+ * Author URI:        https://hillario.github.io
+ * Text Domain:       plugin-slug
+ * License:           Saro Labs
+ * License URI:       https://hillario.github.io
+ * Update URI:        https://hillario.github.io
+ */
+
+?>
